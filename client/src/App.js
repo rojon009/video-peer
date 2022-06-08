01ac674144786d8327123dee659a8af32b59bda8
rojon <rjrojon@gmail.com>
2022-06-08 06:13:25 +0600

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './App.css';
-import io from "socket.io-client";
+import React, { useEffect, useRef, useState } from "react";
 import Peer from "simple-peer";
+import io from "socket.io-client";
 import styled from "styled-components";
+import "./App.css";
 
 const Container = styled.div`
   height: 100vh;
@@ -23,110 +23,133 @@ const Video = styled.video`
 `;
 
 function App() {
+  const [value, setValue] = useState(0);
   const [yourID, setYourID] = useState("");
   const [users, setUsers] = useState({});
-  const [stream, setStream] = useState();
+  const [stream, setStream] = useState(null);
   const [receivingCall, setReceivingCall] = useState(false);
   const [caller, setCaller] = useState("");
-  const [callerSignal, setCallerSignal] = useState();
+  const [callerSignal, setCallerSignal] = useState(null);
   const [callAccepted, setCallAccepted] = useState(false);
 
   const userVideo = useRef();
   const partnerVideo = useRef();
   const socket = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
     socket.current = io.connect("/");
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
       setStream(stream);
       if (userVideo.current) {
         userVideo.current.srcObject = stream;
       }
-    })
+    });
 
     socket.current.on("yourID", (id) => {
       setYourID(id);
-    })
+
+      console.log("me " + id);
+    });
     socket.current.on("allUsers", (users) => {
       setUsers(users);
-    })
+    });
 
     socket.current.on("hey", (data) => {
       setReceivingCall(true);
       setCaller(data.from);
+      console.log("caller " + data.from);
       setCallerSignal(data.signal);
-    })
-  }, []);
+    });
+
+    socket.current.on("callEnded", (data) => {
+      console.log(data);
+      setCallAccepted(false);
+      setCaller("");
+      setCallerSignal(null);
+      setCallAccepted(false);
+      connectionRef.current.destroy();
+      setValue(value + 1);
+      setUsers({});
+    });
+  }, [value]);
 
   function callPeer(id) {
     const peer = new Peer({
       initiator: true,
       trickle: false,
       config: {
-
         iceServers: [
-            {
-                urls: "stun:numb.viagenie.ca",
-                username: "sultan1640@gmail.com",
-                credential: "98376683"
-            },
-            {
-                urls: "turn:numb.viagenie.ca",
-                username: "sultan1640@gmail.com",
-                credential: "98376683"
-            }
-        ]
-    },
+          {
+            urls: "stun:numb.viagenie.ca",
+            username: "sultan1640@gmail.com",
+            credential: "98376683",
+          },
+          {
+            urls: "turn:numb.viagenie.ca",
+            username: "sultan1640@gmail.com",
+            credential: "98376683",
+          },
+        ],
+      },
       stream: stream,
     });
 
-    peer.on("signal", data => {
-      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID })
-    })
+    peer.on("signal", (data) => {
+      socket.current.emit("callUser", { userToCall: id, signalData: data, from: yourID });
+    });
 
-    peer.on("stream", stream => {
+    peer.on("stream", (stream) => {
       if (partnerVideo.current) {
         partnerVideo.current.srcObject = stream;
       }
     });
 
-    socket.current.on("callAccepted", signal => {
+    socket.current.on("callAccepted", (signal) => {
       setCallAccepted(true);
       peer.signal(signal);
-    })
+      setCaller(id);
+    });
 
+    connectionRef.current = peer;
   }
 
   function acceptCall() {
+    setReceivingCall(false);
     setCallAccepted(true);
     const peer = new Peer({
       initiator: false,
       trickle: false,
       stream: stream,
     });
-    peer.on("signal", data => {
-      socket.current.emit("acceptCall", { signal: data, to: caller })
-    })
+    peer.on("signal", (data) => {
+      socket.current.emit("acceptCall", { signal: data, to: caller });
+    });
 
-    peer.on("stream", stream => {
+    peer.on("stream", (stream) => {
       partnerVideo.current.srcObject = stream;
     });
 
     peer.signal(callerSignal);
+    connectionRef.current = peer;
+  }
+
+  function endCall() {
+    setCallAccepted(false);
+    setCaller("");
+    setCallerSignal(null);
+    setCallAccepted(false);
+    setUsers({});
+
+    socket.current.emit("endCall", { to: caller, from: yourID });
+
+    connectionRef.current.destroy();
+    setValue(value + 1);
   }
 
   let UserVideo;
   if (stream) {
-    UserVideo = (
-      <Video playsInline muted ref={userVideo} autoPlay />
-    );
-  }
-
-  let PartnerVideo;
-  if (callAccepted) {
-    PartnerVideo = (
-      <Video playsInline ref={partnerVideo} autoPlay />
-    );
+    UserVideo = <Video playsInline muted ref={userVideo} autoPlay />;
   }
 
   let incomingCall;
@@ -136,27 +159,33 @@ function App() {
         <h1>{caller} is calling you</h1>
         <button onClick={acceptCall}>Accept</button>
       </div>
-    )
+    );
   }
   return (
     <Container>
       <Row>
         {UserVideo}
-        {PartnerVideo}
+        {callAccepted && (
+          <>
+            <Video playsInline ref={partnerVideo} autoPlay />
+            <button onClick={endCall}>End Call</button>
+          </>
+        )}
       </Row>
       <Row>
-        {Object.keys(users).map(key => {
+        {Object.keys(users).map((key) => {
           if (key === yourID) {
             return null;
           }
           return (
-            <button onClick={() => callPeer(key)}>Call {key}</button>
+            <button key={key} onClick={() => callPeer(key)}>
+              Call {key}
+            </button>
           );
         })}
       </Row>
-      <Row>
-        {incomingCall}
-      </Row>
+      <Row>{}</Row>
+      <Row>{receivingCall && !callAccepted && incomingCall && acceptCall()}</Row>
     </Container>
   );
 }
--- server.js
+++ server.js
@@ -6,26 +6,35 @@ const socket = require("socket.io");
 const io = socket(server);
 
 const users = {};
+const peoples = [];
 
-io.on('connection', socket => {
-    if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
-    socket.emit("yourID", socket.id);
+io.on("connection", (socket) => {
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id;
+  }
+
+  socket.emit("yourID", socket.id);
+
+  io.sockets.emit("allUsers", users);
+
+  socket.on("disconnect", function () {
+    console.log("DISCONNESSO!!! ");
+    delete users[socket.id];
     io.sockets.emit("allUsers", users);
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-    })
+  });
 
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-    })
+  socket.on("callUser", (data) => {
+    io.to(data.userToCall).emit("hey", { signal: data.signalData, from: data.from });
+  });
 
-    socket.on("acceptCall", (data) => {
-        io.to(data.to).emit('callAccepted', data.signal);
-    })
+  socket.on("acceptCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
+  });
+
+  socket.on("endCall", (data) => {
+    console.log(data);
+    io.to(data.to).emit("callEnded", data.from);
+  });
 });
 
-server.listen(8000, () => console.log('server is running on port 8000'));
-
-
+server.listen(8000, () => console.log("server is running on port 8000"));
